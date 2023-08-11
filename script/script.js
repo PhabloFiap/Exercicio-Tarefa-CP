b62ae799d3e37d7d298c7ff11daab805af3b1d43
PhabloFiap <rm550687@fiap.com.br>
2023-08-11 16:09:38 -0300

    começando a fazer delete pela img

--- script/script.js
+++ script/script.js
@@ -9,31 +9,45 @@ class Tarefa{
 
     salvar(){
        let tarefa = this.lerDados();
-        
+        console.log(tarefa);
         event.preventDefault();
         this.listaTabela();
-       
+
+        this.adicionar(tarefa);
+        
+        this.listaTabela();
+       this.cancelar();
     }
 
     listaTabela(){
     let tbody = document.getElementById('tbody');
-        tbody.innerText= '';
+        tbody.innerText= '';//pra nao deixar duplicar os itens fazendo vir vazio
         for(let i= 0; i< this.arrayTarefa.length; i++ ){
-        let tr = tbody.insertRow();
+        let tr = tbody.insertRow();//cria uma nova linha dentro da tabela (tbody)
 
-        let tdDescricao = tr.insertCell();
+        let tdDescricao = tr.insertCell();//insere uma nova coluna e insere ao tdDescricao
         let tdAutor = tr.insertCell();
         let tdDepartamento = tr.insertCell(); 
         let tdImportancia= tr.insertCell(); 
-
+        let tdAcao = tr.insertCell();
+//pegando os i pra adicionar os campos
         tdDescricao.innerText = this.arrayTarefa[i].nomeDescricao;
         tdAutor.innerText = this.arrayTarefa[i].nomeAutor;
         tdDepartamento.innerText = this.arrayTarefa[i].nomeDepartamento;
         tdImportancia.innerText = this.arrayTarefa[i].nomeImportancia;
 
-        //  
+        //  adicionar imagem
+
+        let imgEdit = document.createElement('img');
+        imgEdit.src='./img/edit.png';
+        tdAcao.appendChild(imgEdit);
 
 
+        let imgDel = document.createElement('img');
+        imgDel.src= './img/del.png';
+        imgDel.setAttribute("onclick","tarefa.deletar()")
+
+        tdAcao.appendChild(imgDel);
         }
 
     }
@@ -77,8 +91,13 @@ class Tarefa{
     //     return true;
     //   }
     cancelar(){
-        
-
+        document.getElementById('idDescricao').value = '';
+        document.getElementById('idAutor').value ='';
+        document.getElementById('idDepartamento').value ='';
+        document.getElementById('idImportancia').value ='';
+    }
+    deletar(){
+        alert('teste delete'); 
     }
 }
 
